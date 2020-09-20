b9ce09d2c74c21f1690b33f0636db365adf95d8c
Aim4 <aimforking@gmail.com>
2020-09-20 16:11:12 -0400

    Add Gui implementation and hide new game prompt on game start

---
+++ scripts/Gui.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class Gui : Control
+{
+
+    private MarginContainer _scoreContainer;
+    private MarginContainer _newGameContainer;
+
+    public override void _Ready()
+    {
+        _scoreContainer = GetNode<MarginContainer>("Score");
+        _newGameContainer = GetNode<MarginContainer>("NewGame");
+        _newGameContainer.Hide();
+    }
+
+    public void UpdatePlayerScore(int score)
+    {
+
+    }
+
+    public void UpdateEnemyScore(int score)
+    {
+
+    }
+}
